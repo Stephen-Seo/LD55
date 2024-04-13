5366561c583b96ef22fbf66461f6d73b3b95bc0f
Stephen Seo <seo.disparate@gmail.com>
2024-04-13 15:35:28 +0900

    Work
    Added LD55_1.mp3 (Gander's theme).
    
    Added spritesheet for Gander Schwartz (the main character).
    
    More dialoge for introduction.

--- MainLogic.gd
+++ MainLogic.gd
@@ -8,22 +8,41 @@ extends Node2D
 @onready var wind_diamond = $WindDiamond
 @onready var earth_diamond = $EarthDiamond
 
-@onready var audio_stream_player = $AudioStreamPlayer
+@onready var music_player = $MusicPlayer
+
+const text_speed = 0.08
 
 const start_text = "You seek the elementals?\nProve your worth!\nShow the elements your mastery over summoning, and they are yours!"
+
+const intro_text_00 = "The name's Gander Schwartz.\nBut my friends call me the \"Wandering Gander\"."
+const intro_text_01 = "I'm what most would call a \"third-rate summoner\",\nbut there's a reason for that.\nI summon \"items\", not \"beasts\"."
+const intro_text_02 = "Most summoners summon beasts to fight for them.\nI summon items to fight with, or even tools to solve puzzles in dungeons."
 
 const diamond_angle_rate = 1.2
 const diamond_dist_rate = 50.0
 const diamond_start_dist = 800.0
 const diamond_min_dist = 150.0
 
-enum StateT {Start, Start_TextRendered, Start_Stopping, MainMenu}
+enum StateT {
+	Start,
+	Start_TextRendered,
+	Start_Stopping,
+	MainMenu,
+	Introduction_00,
+	Introduction_00_post,
+	Introduction_01,
+	Introduction_01_post,
+	Introduction_02,
+	Introduction_02_post
+}
 
 static var state_dict = {}
 
 var tween_volume
 
 var diamonds_gone = false
+
+var music_file
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -35,21 +54,25 @@ func _ready():
 			"dist": diamond_start_dist,
 			"angle" : 0.0
 		}
-		audio_stream_player.play()
+		music_player.play()
 
+func update_text(text, next_state):
+	if state_dict["timer"] > text_speed:
+		main_label.text += text[state_dict["text_idx"]]
+		state_dict["text_idx"] += 1
+		state_dict["timer"] = 0.0
+		if state_dict["text_idx"] >= text.length():
+			state_dict["state"] = next_state
+			state_dict["text_idx"] = 0
+			state_dict["timer"] = 0.0
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	state_dict["timer"] += delta
 	match state_dict["state"]:
 		StateT.Start:
-			if state_dict["timer"] > 0.15:
-				main_label.text += start_text[state_dict["text_idx"]]
-				state_dict["text_idx"] += 1
-				state_dict["timer"] = 0.0
-				if state_dict["text_idx"] >= start_text.length():
-					state_dict["state"] = StateT.Start_TextRendered
-					state_dict["text_idx"] = 0
+			update_text(start_text, StateT.Start_TextRendered)
 			update_start_diamonds(delta)
 		StateT.Start_TextRendered:
 			update_start_diamonds(delta)
@@ -58,6 +81,35 @@ func _process(delta):
 			update_stop_diamonds(delta)
 		StateT.MainMenu:
 			update_stop_diamonds(delta)
+			state_dict["state"] = StateT.Introduction_00
+			state_dict["timer"] = 0.0
+			state_dict["text_idx"] = 0
+			main_label.text = ""
+			lower_label.text = ""
+			music_player.volume_db = 0.0
+			music_player.stream = AudioStreamMP3.new()
+			music_file = FileAccess.open("res://audio/LD55_1.mp3", FileAccess.READ)
+			music_player.stream.data = music_file.get_buffer(music_file.get_length())
+			music_player.play()
+		StateT.Introduction_00:
+			update_stop_diamonds(delta)
+			update_text(intro_text_00, StateT.Introduction_00_post)
+		StateT.Introduction_00_post:
+			if not diamonds_gone:
+				state_dict["start_diamonds"]["dist"] = diamond_start_dist
+				diamonds_gone = true
+				fire_diamond.get_parent().remove_child(fire_diamond)
+				water_diamond.get_parent().remove_child(water_diamond)
+				wind_diamond.get_parent().remove_child(wind_diamond)
+				earth_diamond.get_parent().remove_child(earth_diamond)
+		StateT.Introduction_01:
+			update_text(intro_text_01, StateT.Introduction_01_post)
+		StateT.Introduction_01_post:
+			pass
+		StateT.Introduction_02:
+			update_text(intro_text_02, StateT.Introduction_02_post)
+		StateT.Introduction_02_post:
+			pass
 		_:
 			pass
 			
@@ -72,15 +124,40 @@ func _unhandled_input(event):
 			StateT.Start_TextRendered:
 				state_dict["state"] = StateT.Start_Stopping
 				tween_volume = get_tree().create_tween()
-				tween_volume.tween_property(audio_stream_player, "volume_db", -80.0, 4.0)
+				tween_volume.tween_property(music_player, "volume_db", -80.0, 4.0)
 				tween_volume.tween_callback(start_volume_tween_callback)
 				main_label.text = ""
 				lower_label.text = ""
+			StateT.Introduction_00:
+				main_label.text = intro_text_00
+				state_dict["text_idx"] = 0
+				state_dict["timer"] = 0.0
+				state_dict["state"] = StateT.Introduction_00_post
+			StateT.Introduction_00_post:
+				state_dict["state"] = StateT.Introduction_01
+				state_dict["timer"] = 0.0
+				state_dict["text_idx"] = 0
+				main_label.text = ""
+			StateT.Introduction_01:
+				state_dict["state"] = StateT.Introduction_01_post
+				state_dict["timer"] = 0.0
+				state_dict["text_idx"] = 0
+				main_label.text = intro_text_01
+			StateT.Introduction_01_post:
+				state_dict["state"] = StateT.Introduction_02
+				state_dict["timer"] = 0.0
+				state_dict["text_idx"] = 0
+				main_label.text = ""
+			StateT.Introduction_02:
+				state_dict["state"] = StateT.Introduction_02_post
+				state_dict["timer"] = 0.0
+				state_dict["text_idx"] = 0
+				main_label.text = intro_text_02
 			_:
 				pass
 
 func start_volume_tween_callback():
-	audio_stream_player.stop()
+	music_player.stop()
 	state_dict["state"] = StateT.MainMenu
 	
 func diamond_position_update():
